8ef1611f53a8e50fa0f0061262f7100c6f25712c
RedPandaMC <jurreandenys@gmail.com>
2023-03-21 12:41:08 +0100

    Joepie! Eerste commit is een feit!

---
+++ main.py
@@ -0,0 +1,9 @@
+#cock??
+"""this code is a test"""
+def som(a:int,b:int):
+    """
+    Deze functie neemt een integer a en een integer b, \
+    returns de som van deze twee integers
+    """
+    return a + b
+    
